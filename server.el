037655bcacddcc9a50501533ae0d742aab07fb42
nothing-c <halskdjf@gmail.com>
2025-01-15 20:32:48 -0600

    get alist rolling + get org file matcher worked out

--- server.el
+++ server.el
@@ -3,6 +3,7 @@
 
 (defvar eos-handlers
   '(((:GET . "/foo") . eos-index)
+    ((:GET . "\.org") . eos-org-file)
     ))
 
 (defun eos-index (request)
@@ -10,6 +11,23 @@
   (with-slots (process headers) request
     (ws-response-header process 200 '("Content-type" . "text/html"))
     (process-send-string (process request) "<html><head>Index</head><body><p>Hi!</p></body></html>")))
+
+(defun eos-org-file (request)
+  "Generate and serve an org-mode file"
+  (with-slots (process headers) request
+    (ws-response-header process 200 '("Content-type" . "text/html"))
+    (process-send-string (process request) (eos-render-org-file (eos-get-file headers)))))
+
+(defun eos-get-file (headers)
+  "Get the name of the requested file"
+  "./elisp-org-server.org")		; Temporary
+
+(defun eos-render-org-file (file)
+  "Render an org file into a string of HTML"
+  (with-temp-buffer
+    (insert-file-contents file)
+    (org-export-to-buffer 'html "*auto-org-export*" '() '() '() t)
+    (buffer-string)))
 
 
 ;; (ws-start				; Working
