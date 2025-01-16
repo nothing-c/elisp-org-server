ca359d4f77e6fae0e656c5d886aff281798f78c7
nothing-c <halskdjf@gmail.com>
2025-01-15 20:16:33 -0600

    Getting it rolling, having some difficulty with the handler matching

--- server.el
+++ server.el
@@ -1,4 +1,16 @@
 (require 'web-server)
+(defvar eos-port 9000)			;Temporary
+
+(defvar eos-handlers
+  '(((:GET . "/foo") . eos-index)
+    ))
+
+(defun eos-index (request)
+  "Generate tmp index.html"
+  (with-slots (process headers) request
+    (ws-response-header process 200 '("Content-type" . "text/plain"))
+    (process-send-string (process request) "<html><head>Index</head><body><p>Hi!</p></body></html>")))
+
 
 ;; (ws-start				; Working
 ;;  (lambda (request)
@@ -7,3 +19,6 @@
 ;;      (process-send-string process "hello world")))
 ;;  9000)
 
+(defun eos-run ()
+  "Run the elisp org server (name not final)"
+  (ws-start eos-handlers eos-port))
